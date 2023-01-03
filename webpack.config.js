aa2701fe34bcb874806972ac57bbf4d3c27b2f05
gacetta <78240758+gacetta@users.noreply.github.com>
2023-01-02 16:17:52 -0800

    feat: created boilerplate for budget app

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          test: /\.js$/,
+          exclude: /node_modules/
+        }
+      }]
+    }, {
+      test: /\.s?css$/i,
+      use: [
+        "style-loader", 
+        "css-loader",
+        "sass-loader"
+      ]
+    }]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    }
+  },
+  mode: 'development'
+};
